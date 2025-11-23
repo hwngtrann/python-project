9684d1cdf54a8c1164c22bf946f30f057140211d
hwngtrann <htran3@usf.edu>
2025-11-23 17:14:49 -0500

    Add calculation for estimated weekly productivity

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 print("Welcome! This is my Productivity Calculator.")
 
 task_count = input("How many tasks did you complete today? ")
+# Convert to number
+task_count = float(task_count)
+
+# Estimate weekly productivity
+weekly_tasks = task_count * 7
